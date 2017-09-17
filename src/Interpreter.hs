435657dac400a5c095bc41062856dbdb95a6fd1c
Peter Uhlenbruck <peuhlenb@gmail.com>
2017-09-17 02:19:24 -0400

    Switch storage of program commands from List to Vector
    By using Vector witha program counter instead of List we can easily seek
    backwards to previous commands which is required to implement ]

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -4,16 +4,33 @@ module Interpreter
 
 import Data.Maybe
 import Data.Word
+import Data.Vector (Vector, (!))
+import qualified Data.Vector as Vector
 import System.IO
 
 run :: [Char] -> IO()
-run = runProgram . mapMaybe parseCommand
+run = runProgram . Vector.fromList . mapMaybe parseCommand
 
 data Command = DEBUG | NextCell | PrevCell | Increment | Decrement | Write | Read | ForwardJumpIfZero | BackWardJumpIfNonZero
     deriving (Show)
 
-data DataPointer = DataPointer [Word8] Word8 [Word8]
+data DataPointer = DataPointer {previousCells :: [Word8], currentCell :: Word8, nextCells :: [Word8]}
     deriving (Show)
+
+nextCell :: DataPointer -> DataPointer
+nextCell (DataPointer previous current [])     = DataPointer (current : previous) 0 []
+nextCell (DataPointer previous current (n:ns)) = DataPointer (current : previous) n ns
+
+prevCell :: DataPointer -> DataPointer
+prevCell (DataPointer [] current next) = DataPointer [] 0 (current : next)
+prevCell (DataPointer (p:ps) current next) = DataPointer ps p (current : next)
+
+incrementCurrent :: DataPointer -> DataPointer
+incrementCurrent (DataPointer previous current next) = DataPointer previous (current + 1) next
+
+decrementCurrent :: DataPointer -> DataPointer
+decrementCurrent (DataPointer previous current next) = DataPointer previous (current - 1) next
+
 
 parseCommand :: Char -> Maybe Command
 parseCommand '>' = Just NextCell
@@ -27,12 +44,26 @@ parseCommand ']' = Just BackWardJumpIfNonZero
 parseCommand '*' = Just DEBUG
 parseCommand  _  = Nothing
 
-runProgram :: [Command] -> IO()
-runProgram [] = return ()
-runProgram (x:xs) = loop (x : xs) (DataPointer [] 0 [])
+runProgram :: Vector Command -> IO()
+runProgram code
+    | Vector.null code = return ()
+    | otherwise = loop code 0 (DataPointer [] 0 [])
 
-loop :: [Command] -> DataPointer -> IO()
-loop [] _ = return ()
+loop :: Vector Command -> Int -> DataPointer -> IO()
+loop commands pc dataPointer
+    | pc >= Vector.length commands = return ()
+    | otherwise            = let (newPC, newDataPointer, action) = processCommand (commands ! pc) commands pc dataPointer
+                             in action >> loop commands newPC newDataPointer
+
+
+processCommand :: Command -> Vector Command -> Int -> DataPointer -> (Int, DataPointer, IO ())
+processCommand NextCell  commands pc dataPointer = (pc + 1, nextCell dataPointer, return ())
+processCommand PrevCell  commands pc dataPointer = (pc + 1, prevCell dataPointer, return ())
+processCommand Increment commands pc dataPointer = (pc + 1, incrementCurrent dataPointer, return ())
+processCommand Decrement commands pc dataPointer = (pc + 1, decrementCurrent dataPointer, return ())
+processCommand Write     commands pc dataPointer = (pc + 1, dataPointer, putChar (toEnum . fromEnum $ currentCell dataPointer) >> hFlush stdout)
+processCommand DEBUG     commands pc dataPointer = (pc + 1, dataPointer, print dataPointer >> hFlush stdout)
+    {--
 loop (NextCell:xs) (DataPointer previous current []) = loop xs (DataPointer (current : previous) 0 [])
 loop (NextCell:xs) (DataPointer previous current (n:ns)) = loop xs (DataPointer (current : previous) n ns)
 loop (PrevCell:xs) (DataPointer [] current next) = loop xs (DataPointer [] 0 (current : next))
@@ -41,3 +72,4 @@ loop (Increment:xs) (DataPointer previous current next) = loop xs (DataPointer p
 loop (Decrement:xs) (DataPointer previous current next) = loop xs (DataPointer previous (current - 1) next)
 loop (Write:xs) (DataPointer previous current next) = putChar(toEnum . fromEnum $ current) >> hFlush stdout >> loop xs (DataPointer previous current next)
 loop (DEBUG:xs) state = print state >> hFlush stdout >> loop xs state
+--}
