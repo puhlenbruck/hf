d1542b8bc7a11d974411b408a9a7def264bf1029
Peter Uhlenbruck <peuhlenb@gmail.com>
2017-09-17 16:21:16 -0400

    Remove commented code

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -35,8 +35,8 @@ runProgram code
 loop :: Vector Command -> Int -> DataPointer -> IO()
 loop commands pc dataPointer
     | pc >= Vector.length commands = return ()
-    | otherwise            = let (newPC, newDataPointer, action) = processCommand (commands ! pc) commands pc dataPointer
-                             in action >> loop commands newPC newDataPointer
+    | otherwise = let (newPC, newDataPointer, action) = processCommand (commands ! pc) commands pc dataPointer
+                  in action >> loop commands newPC newDataPointer
 
 processCommand :: Command -> Vector Command -> Int -> DataPointer -> (Int, DataPointer, IO ())
 processCommand NextCell  commands pc dataPointer = (pc + 1, nextCell dataPointer, return ())
@@ -45,13 +45,3 @@ processCommand Increment commands pc dataPointer = (pc + 1, incrementCurrent dat
 processCommand Decrement commands pc dataPointer = (pc + 1, decrementCurrent dataPointer, return ())
 processCommand Write     commands pc dataPointer = (pc + 1, dataPointer, putChar (toEnum . fromEnum $ currentCell dataPointer) >> hFlush stdout)
 processCommand DEBUG     commands pc dataPointer = (pc + 1, dataPointer, print dataPointer >> hFlush stdout)
-    {--
-loop (NextCell:xs) (DataPointer previous current []) = loop xs (DataPointer (current : previous) 0 [])
-loop (NextCell:xs) (DataPointer previous current (n:ns)) = loop xs (DataPointer (current : previous) n ns)
-loop (PrevCell:xs) (DataPointer [] current next) = loop xs (DataPointer [] 0 (current : next))
-loop (PrevCell:xs) (DataPointer (p:ps) current next) = loop xs (DataPointer ps p (current : next))
-loop (Increment:xs) (DataPointer previous current next) = loop xs (DataPointer previous (current + 1) next)
-loop (Decrement:xs) (DataPointer previous current next) = loop xs (DataPointer previous (current - 1) next)
-loop (Write:xs) (DataPointer previous current next) = putChar(toEnum . fromEnum $ current) >> hFlush stdout >> loop xs (DataPointer previous current next)
-loop (DEBUG:xs) state = print state >> hFlush stdout >> loop xs state
---}
